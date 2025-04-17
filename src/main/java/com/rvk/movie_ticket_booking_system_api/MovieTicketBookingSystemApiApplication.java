bfe5f0c6c3c1691ee539f700f295adabb75e5bd1
veeraskumar <veeraskumar000@gmail.com>
2025-04-17 07:32:09 -0400

    movie-ticket-booking-system-api version-000

---
+++ src/main/java/com/rvk/movie_ticket_booking_system_api/MovieTicketBookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.rvk.movie_ticket_booking_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rvk/movie_ticket_booking_system_api/MovieTicketBookingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvk.movie_ticket_booking_system_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
