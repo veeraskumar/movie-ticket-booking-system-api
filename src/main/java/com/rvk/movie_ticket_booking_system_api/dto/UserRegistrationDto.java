8025c5e0d1d113174970620c40b0708583cafd2b
veeraskumar <veeraskumar000@gmail.com>
2025-04-23 08:24:07 -0400

    Vaild and for Error excption

--- src/main/java/com/rvk/movie_ticket_booking_system_api/dto/UserRegistrationDto.java
+++ src/main/java/com/rvk/movie_ticket_booking_system_api/dto/UserRegistrationDto.java
@@ -1,15 +1,25 @@
 package com.rvk.movie_ticket_booking_system_api.dto;
 
 import com.rvk.movie_ticket_booking_system_api.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record UserRegistrationDto(
+        @NotNull
         String userName,
+        @NotNull @Email
         String email,
+        @NotNull
         String phoneNumber,
+        @NotNull
         String password,
+        @NotNull
         Role userRole,
+        @NotNull
         LocalDate dateOfBirth
 ) {
 }
